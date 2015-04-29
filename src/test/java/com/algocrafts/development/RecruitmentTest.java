559a9f28e79c5b4546e059bd0da201e016580c54
Yujun Liang <yujunliang@hotmail.com>
2015-04-29 14:08:41 -0500

    Added travis.yml file.

--- src/test/java/com/algocrafts/development/RecruitmentTest.java
+++ src/test/java/com/algocrafts/development/RecruitmentTest.java
@@ -1,5 +1,6 @@
 package com.algocrafts.development;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -15,16 +16,23 @@ import static org.junit.Assert.assertEquals;
 public class RecruitmentTest {
 
 
-    @Test
-    public void testHire() throws Exception {
+    private Recruitment recruitment;
+
+    @Before
+    public void before() {
         List<Developer> candidates = new ArrayList<Developer>();
-        candidates.add(new Developer("Jack", EnumSet.of(JAVA,C ), null, null, null));
-        candidates.add(new Developer("Bob", EnumSet.of(COBOL,C, C_SHARP), null, null, null));
+        candidates.add(new Developer("Jack", EnumSet.of(JAVA, C), null, null, null));
+        candidates.add(new Developer("Bob", EnumSet.of(COBOL, C, C_SHARP), null, null, null));
         candidates.add(new Developer("Sanjay", EnumSet.of(JAVA, CPP), null, null, null));
         candidates.add(new Developer("Weidong", EnumSet.of(JAVA, C_SHARP), null, null, null));
         candidates.add(new Developer("Wendy", EnumSet.of(JAVA, C_SHARP), null, null, null));
 
-        Recruitment recruitment = new Recruitment(candidates);
+        recruitment = new Recruitment(candidates);
+
+    }
+
+    @Test
+    public void testHire() throws Exception {
         List<Developer> hired = recruitment.hire(JAVA, C_SHARP);
         assertEquals(2, hired.size());
         assertEquals("Weidong", hired.get(0).getName());
@@ -33,14 +41,6 @@ public class RecruitmentTest {
 
     @Test
     public void testHire2() throws Exception {
-        List<Developer> candidates = new ArrayList<Developer>();
-        candidates.add(new Developer("Jack", EnumSet.of(JAVA, C), null, null, null));
-        candidates.add(new Developer("Bob", EnumSet.of(COBOL,C, C_SHARP), null, null, null));
-        candidates.add(new Developer("Sanjay", EnumSet.of(JAVA, CPP), null, null, null));
-        candidates.add(new Developer("Weidong", EnumSet.of(JAVA, C_SHARP), null, null, null));
-        candidates.add(new Developer("Wendy", EnumSet.of(JAVA, C_SHARP), null, null, null));
-
-        Recruitment recruitment = new Recruitment(candidates);
         List<Developer> hired = recruitment.hire2(JAVA, C_SHARP);
         assertEquals(2, hired.size());
         assertEquals("Weidong", hired.get(0).getName());
@@ -49,14 +49,6 @@ public class RecruitmentTest {
 
     @Test
     public void testHire3() throws Exception {
-        List<Developer> candidates = new ArrayList<Developer>();
-        candidates.add(new Developer("Jack", EnumSet.of(JAVA, C), null, null, null));
-        candidates.add(new Developer("Bob", EnumSet.of(COBOL,C, C_SHARP), null, null, null));
-        candidates.add(new Developer("Sanjay", EnumSet.of(JAVA, CPP), null, null, null));
-        candidates.add(new Developer("Weidong", EnumSet.of(JAVA, C_SHARP), null, null, null));
-        candidates.add(new Developer("Wendy", EnumSet.of(JAVA, C_SHARP), null, null, null));
-
-        Recruitment recruitment = new Recruitment(candidates);
         List<Developer> hired = recruitment.hire3(JAVA, C_SHARP);
         assertEquals(2, hired.size());
         assertEquals("Weidong", hired.get(0).getName());
@@ -65,14 +57,6 @@ public class RecruitmentTest {
 
     @Test
     public void testHire4() throws Exception {
-        List<Developer> candidates = new ArrayList<Developer>();
-        candidates.add(new Developer("Jack", EnumSet.of(JAVA, C), null, null, null));
-        candidates.add(new Developer("Bob", EnumSet.of(COBOL,C, C_SHARP), null, null, null));
-        candidates.add(new Developer("Sanjay", EnumSet.of(JAVA, CPP), null, null, null));
-        candidates.add(new Developer("Weidong", EnumSet.of(JAVA, C_SHARP), null, null, null));
-        candidates.add(new Developer("Wendy", EnumSet.of(JAVA, C_SHARP), null, null, null));
-
-        Recruitment recruitment = new Recruitment(candidates);
         Stream<Developer> hired = recruitment.hire4(JAVA, C_SHARP);
         assertEquals(2, hired.count());
         List<Developer> list = hired.collect(Collectors.<Developer>toList());
